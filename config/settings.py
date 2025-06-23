69a3603c09dba57a0b210a884b0d1009ef946567
tjhootman <tjhootman@gmail.com>
2025-06-22 21:56:40 -0400

    Add views and templates for password reset

--- config/settings.py
+++ config/settings.py
@@ -127,5 +127,9 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = 'dashboard'
+
 LOGIN_URL = 'login'
+
 LOGOUT_URL = 'logout'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
