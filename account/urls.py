8c328bdb83c4538dfb027a551806a8ba924baa73
tjhootman <tjhootman@gmail.com>
2025-06-22 22:15:44 -0400

    Add views for new user registration

--- account/urls.py
+++ account/urls.py
@@ -5,6 +5,8 @@ from . import views
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),
     path('', include('django.contrib.auth.urls')),
+    path('register/', views.register, name='register'),
+
     # path('login/', auth_views.LoginView.as_view(), name='login'),
     # path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     # change password urls
