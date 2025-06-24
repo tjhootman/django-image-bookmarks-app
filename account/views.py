a4ced536d0467a78ce2dadc1979e175746b76a62
tjhootman <tjhootman@gmail.com>
2025-06-24 10:54:53 -0400

    Add messages for updating profile

--- account/views.py
+++ account/views.py
@@ -2,9 +2,9 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
-from .forms import LoginForm, UserRegistrationForm
-from .models import Profile
+from django.contrib import messages
 from .forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
+from .models import Profile
 
 def user_login(request):
     if request.method == 'POST':
@@ -58,8 +58,7 @@ def register(request):
 @login_required
 def edit(request):
     if request.method == 'POST':
-        user_form = UserEditForm(instance=request.user,
-                                 data=request.POST)
+        user_form = UserEditForm(instance=request.user, data=request.POST)
         profile_form = ProfileEditForm(
                                     instance=request.user.profile,
                                     data=request.POST,
@@ -67,11 +66,12 @@ def edit(request):
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
+            messages.success(request, 'Profile updated successfully')
+        else:
+            messages.error(request, 'Error updating your profile')
     else:
         user_form = UserEditForm(instance=request.user)
-        profile_form = ProfileEditForm(
-                                    instance=request.user.profile)
+        profile_form = ProfileEditForm(instance=request.user.profile)
     return render(request,
                   'account/edit.html',
-                  {'user_form': user_form,
-                   'profile_form': profile_form})
+                  {'user_form': user_form, 'profile_form': profile_form})
