10b8148d07693d80494083b02c566ce248caabdb
tjhootman <tjhootman@gmail.com>
2025-07-20 17:36:31 -0400

    Add functions, views and templates for creating image bookmarks

--- config/urls.py
+++ config/urls.py
@@ -22,7 +22,10 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/', include('account.urls')),
+    path('social-auth/', include('social_django.urls', namespace='social')),
+    path('images/', include('images.urls', namespace='images')),
 ]
+
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
                         document_root=settings.MEDIA_ROOT)
